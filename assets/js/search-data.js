565457be08daecd34964568ce8969fa8fbda75a7
karinorman <kari@normanfamily.org>
2026-06-01 20:26:20 +0000

    Deploying to gh-pages from @ karinorman/karinorman.github.io@711f0a5088838c9a3c7769dce0348636c716a74a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "PDF and webpage last updated 11/2025.",
+          description: "PDF and webpage last updated 6/2026.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
